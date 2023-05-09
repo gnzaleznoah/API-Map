a9d57967e0a46ed3d60cf117ef1de1f9cb26f75d
Noah <gnzalezmnoah@gmail.com>
2023-05-09 11:17:51 -0700

    update changes

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-        //create map container
+var locations = [
+        ["LOCATION_1", 37.929810353187975, -122.0748552425516],
+        ["LOCATION_2",38.019047, -121.898251],
+        ["LOCATION_3", 37.806325, -121.672724],
+        ["LOCATION_4", 37.87239738738917, -122.22062356735398],
+        ["LOCATION_5", 37.967373, -121.735475],
+        ["LOCATION_6", 38.013826, -121.757202],
+        ["LOCATION_7", 37.805238, -122.333107],
+        ["LOCATION_8", 37.837321, -122.315151]
+];
+       //create map container
         var map = L.map('map').setView([37.89107798661859, -121.91759862164699], 10.25);
     
         //add map image tyles to map 
@@ -7,15 +17,9 @@
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-        var locations = [
-                [37.929810353187975, -122.0748552425516],
-                [38.019047, -121.898251],
-                [37.806325, -121.672724],
-                [37.87239738738917, -122.22062356735398],
-        ];
 
         for (var i =0; i<locations.length; i++){
-                marker = new L.marker;([locations[i][1],locations[i][2]])
+                marker = new L.marker([locations[i][1],locations[i][2]])
                 .bindPopup(locations[i][0])
                 .addTo(map);
         }
